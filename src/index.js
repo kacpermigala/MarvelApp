70e7b2aec76af850b2043adf94938e14eee1bc6e
Kacper Migala <kacper.migala@infopark.de>
2019-09-16 11:17:01 +0200

    Edit readme & add polyfills

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import polyfills from './polyfills.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
